d70b87e81671847ec08cec4ef930bcee4b33031b
hlpcaptain <newlywedkim@naver.com>
2019-11-07 15:37:15 +0900

    공지사항및자료실 마크업

--- js/webcafe.js
+++ js/webcafe.js
@@ -1,12 +1,17 @@
 var menu_item = $('.menu > li');
 var btn_menu = $('.menu > li > a');
 var sub_link = $('.sub-menu a');
+var tab_section = $('.board section');
+var tab = $('.tab a');
+var tab_list_link = $('.board li a')
 
 menu_item.addClass('menu-item');
 btn_menu.addClass('btn-menu');
 // sub_link.attr('class', 'icon-dot-circled');
 sub_link.addClass('icon-dot-circled');
+tab_list_link.addClass('icon-dot-circled');
 
+//메인 메뉴
 btn_menu.click(function(e){
   var current_class = $(this).parent().hasClass('menu-act');
   e.preventDefault();
@@ -17,5 +22,12 @@ btn_menu.click(function(e){
   }else {
     $(this).parent().addClass('menu-act');
   }
-
 });
+//탭 메뉴
+tab.click(function(e){
+  e.preventDefault; //기본이벤트 취소 의미
+  tab_section.removeClass('tab-act');
+  $(this).parent().parent().addClass('tab-act');
+}
+
+);
